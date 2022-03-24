1923d5071266002562ec4d6c7c23a62f221faa8c
Pamela Badermann <badermann.pamela@gmail.com>
2022-03-24 20:38:47 -0300

    dragula com ng-repeat

--- js/dragAndDropCtrl.js
+++ js/dragAndDropCtrl.js
@@ -3,22 +3,19 @@ angular.module("dragAndDrop").controller("dragAndDropCtrl", function ($scope,) {
     $scope.app = "Drag and drop with AngularJS and Dragula";
 
     $scope.init = function () {
-        dragula([document.querySelector('#dragparent'), document.querySelector('#dragparent1'), document.querySelector('#dragparent2')]);
+        dragula([
+            document.querySelector('#dragparent'),
+            document.querySelector('#dragparent1'),
+            document.querySelector('#dragparent2'),
+            document.querySelector('#dragparent3'),
+        ]);
     };
 
-    $scope.itemsToDrag = [{
-        "Lorem Ipsum ": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod urna at nunc tincidunt, euismod eleifend purus feugiat. "
-    }, {
-        "Lorem Ipsum ": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod urna at nunc tincidunt, euismod eleifend purus feugiat. "
-    }, {
-        "Lorem Ipsum ": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod urna at nunc tincidunt, euismod eleifend purus feugiat. "
-    }, {
-        "Lorem Ipsum ": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod urna at nunc tincidunt, euismod eleifend purus feugiat. "
-    }, {
-        "Lorem Ipsum ": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod urna at nunc tincidunt, euismod eleifend purus feugiat. "
-    }, {
-        "Lorem Ipsum ": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod urna at nunc tincidunt, euismod eleifend purus feugiat. "
-    }, {
-        "Lorem Ipsum ": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod urna at nunc tincidunt, euismod eleifend purus feugiat. "
-    }];
+    $scope.many = ['The', 'possibilities', 'are', 'endless!', 'Explore', 'them'];
+
+    $scope.$on('dragparent3.drop', function (e, el, target, source) {
+        if (target[0].id == 'done') {
+            console.log('done');
+        }
+    });
 });
